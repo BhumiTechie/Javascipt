4f4ef0a8a4409b7f757658f28fdcbc4c589f3ece
Bhumika gupta <bg0674079@gmail.com>
2024-04-05 22:01:01 +0530

    first

---
+++ index.js
@@ -0,0 +1,8 @@
+// First class Function 
+
+// # ASsigning a function to a variable !
+const hello= () =>{
+    console.log("Hello JS!")
+}
+
+hello(); //We assigned an Anonymous Function in a Variable, then we used that variable to invoke the function by adding parentheses () at the end.
